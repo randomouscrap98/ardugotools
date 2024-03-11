5ad4b963a17a1cda3d4005b353627a776ce59954
Carlos Sanchez <randomouscrap98@aol.com>
2024-03-11 01:17:17 -0400

    Switched to KONG

--- go.mod
+++ go.mod
@@ -3,12 +3,12 @@ module github.com/randomouscrap98/ardugotools
 go 1.22.0
 
 require (
-	github.com/urfave/cli/v3 v3.0.0-alpha9
+	github.com/alecthomas/kong v0.9.0
 	go.bug.st/serial v1.6.2
 )
 
 require (
 	github.com/creack/goselect v0.1.2 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"log"
-	"os"
 
+	"github.com/alecthomas/kong"
 	"github.com/randomouscrap98/ardugotools/arduboy"
-	"github.com/urfave/cli/v3"
 )
 
 // Quick way to fail on error, since most commands are "doing" something on
@@ -19,146 +17,68 @@ func fatalIfErr(subject string, doing string, err error) {
 	}
 }
 
-// Scan for arduboys and return json
-func scanAction() {
-	devices, err := arduboy.GetBasicDevices()
-	fatalIfErr("scan", "pull devices", err)
-	PrintJson(devices)
-}
-
 func connectWithBootloader(device string) (io.ReadWriteCloser, *arduboy.BasicDeviceInfo) {
 	sercon, d, err := arduboy.ConnectWithBootloader(device)
 	fatalIfErr(device, "connect", err)
 	return sercon, d
 }
 
-func queryAction(device string) {
-	sercon, d := connectWithBootloader(device)
-	extdata, err := arduboy.QueryDevice(d, sercon)
-	fatalIfErr(device, "query device information", err)
-	PrintJson(extdata)
+// Scan command
+type ScanCmd struct {
 }
 
-func sketchReadAction(device string, filename string) {
+func (c *ScanCmd) Run() error {
+	devices, err := arduboy.GetBasicDevices()
+	fatalIfErr("scan", "pull devices", err)
+	PrintJson(devices)
+	return nil
+}
+
+// Query command
+type QueryCmd struct {
+	Device string `arg:"" help:"The system device to check (use 'any' for first)"`
+}
+
+func (c *QueryCmd) Run() error {
+	sercon, d := connectWithBootloader(c.Device)
+	extdata, err := arduboy.QueryDevice(d, sercon)
+	fatalIfErr(c.Device, "query device information", err)
+	PrintJson(extdata)
+	return nil
+}
+
+// Sketch read command
+type SketchReadCmd struct {
+	Device  string `arg:"" help:"The system device to read from (use 'any' for first)"`
+	Outfile string `short:"o"`
+}
+
+func (c *SketchReadCmd) Run() error {
 	// Read sketch First
-	sercon, _ := connectWithBootloader(device)
+	sercon, _ := connectWithBootloader(c.Device)
 	sketch, err := arduboy.ReadSketch(sercon)
-	fatalIfErr(device, "read sketch", err)
+	fatalIfErr(c.Device, "read sketch", err)
 	hash := arduboy.Md5String(sketch)
-	if filename == "" {
-		filename = fmt.Sprintf("%s_%s.hex", hash, FileSafeDateTime())
+	if c.Outfile == "" {
+		c.Outfile = fmt.Sprintf("%s_%s.hex", hash, FileSafeDateTime())
 	}
 	result := make(map[string]interface{})
-	result["Filename"] = filename
+	result["Filename"] = c.Outfile
 	result["MD5"] = hash
 	PrintJson(result)
+	return nil
+}
+
+var cli struct {
+	Scan   ScanCmd  `cmd:"" help:"Search for Arduboys and return basic information on them"`
+	Query  QueryCmd `cmd:"" help:"Get deeper information about a particular Arduboy"`
+	Sketch struct {
+		Read SketchReadCmd `cmd:"" help:"Read just the sketch portion of flash, saved as a .hex file"`
+	} `cmd:"" help:"Perform actions on the builtin flash or related to sketch files"`
 }
 
 func main() {
-	// Used this example: https://github.com/urfave/cli/blob/main/docs/v3/examples/subcommands.md
-	// And this: https://github.com/urfave/cli/blob/main/docs/v3/examples/combining-short-options.md
-	cmd := &cli.Command{
-		Name:  "ardugotools",
-		Usage: "A set of commands for working Arduboy on the command line",
-		Description: "This is a reimplementation of the Arduboy Toolset GUI as a command line tool. " +
-			"It is designed primarily to be used inside scripts, so some output may not (yet) have nice formatting. " +
-			"You can use 'any' in place of any [port] argument to connect to the first arduboy found",
-		Version: "0.1.0",
-		Authors: []any{"haloopdy"},
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "outfile",
-				Aliases: []string{"o"},
-				Usage:   "Save to `FILE`",
-			},
-			&cli.StringFlag{
-				Name:    "infile",
-				Aliases: []string{"i"},
-				Usage:   "Read from `FILE`",
-			},
-		},
-		Commands: []*cli.Command{
-			{
-				Name: "scan",
-				//Aliases: []string{"a"},
-				Usage: "scan for all connected arduboys and return basic info without querying the device",
-				Description: "This command queries your operating system for all serial ports and pulls whatever " +
-					"data it can without opening the ports or reading/writing over serial. Different operating systems " +
-					"provide different amounts of data. An internal VID/PID table is used to lookup the board; if your device " +
-					"isn't showing up, it may not yet exist in the table.",
-				ArgsUsage: "",
-				Action: func(ctx context.Context, cmd *cli.Command) error {
-					scanAction()
-					return nil
-				},
-			},
-			{
-				Name: "query",
-				//Aliases: []string{"a"},
-				Usage: "query an individual arduboy for more information (may reboot device)",
-				Description: "Connect to the given device, querying as much as possible about it. Certain fields, " +
-					"like the version, are exact, while others, like the apparent device and bootloader size, are " +
-					"a best guess. This command can help you identify the capabilities of the connected Arduboy.",
-				ArgsUsage: "[port]",
-				Action: func(ctx context.Context, cmd *cli.Command) error {
-					queryAction(cmd.Args().First())
-					return nil
-				},
-			},
-			{
-				Name:  "sketch",
-				Usage: "Commands for working with arduboy sketches",
-				Commands: []*cli.Command{
-					{
-						Name:  "read",
-						Usage: "read the sketch as-is from the arduboy as a .hex file",
-						Description: "Connect to the given device and read the entire sketch contained within the " +
-							"flash memory. A default filename is chosen if none is provided, otherwise you can do -o <filename>. " +
-							"Files are written as .hex, which is the universal format tools expect to write back to arduboy",
-						ArgsUsage: "[port]",
-						Action: func(ctx context.Context, cmd *cli.Command) error {
-							sketchReadAction(cmd.Args().First(), cmd.String("outfile"))
-							return nil
-						},
-					},
-				},
-			},
-			//{
-			//	Name:    "complete",
-			//	Aliases: []string{"c"},
-			//	Usage:   "complete a task on the list",
-			//	Action: func(ctx context.Context, cmd *cli.Command) error {
-			//		fmt.Println("completed task: ", cmd.Args().First())
-			//		return nil
-			//	},
-			//},
-			//{
-			//	Name:    "template",
-			//	Aliases: []string{"t"},
-			//	Usage:   "options for task templates",
-			//	Commands: []*cli.Command{
-			//		{
-			//			Name:  "add",
-			//			Usage: "add a new template",
-			//			Action: func(ctx context.Context, cmd *cli.Command) error {
-			//				fmt.Println("new task template: ", cmd.Args().First())
-			//				return nil
-			//			},
-			//		},
-			//		{
-			//			Name:  "remove",
-			//			Usage: "remove an existing template",
-			//			Action: func(ctx context.Context, cmd *cli.Command) error {
-			//				fmt.Println("removed task template: ", cmd.Args().First())
-			//				return nil
-			//			},
-			//		},
-			//	},
-			//},
-		},
-	}
-
-	if err := cmd.Run(context.Background(), os.Args); err != nil {
-		log.Fatal(err)
-	}
+	ctx := kong.Parse(&cli)
+	err := ctx.Run()
+	ctx.FatalIfErrorf(err)
 }
