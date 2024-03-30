6474533dded455a6921fdd4a5eee0a723c865ea8
Carlos Sanchez <randomouscrap98@aol.com>
2024-03-30 18:48:36 -0400

    Set 386 build to way lower requirement

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cd ..
 build() {
 	builddir=build/${1}_${2}
 	mkdir -p $builddir
-	GOOS=$1 GOARCH=$2 go build -o $builddir/ardugotools
+	GOOS=$1 GOARCH=$2 GO386=softfloat go build -o $builddir/ardugotools
 	echo "Compiled $builddir"
 
 	distdir=dist
