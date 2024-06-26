c03983a037bfee6e2f0a3c3506d52f95f783cb57
Carlos Sanchez <randomouscrap98@aol.com>
2024-06-26 02:50:23 -0400

    Working gentitle script

--- helpers/gentitle.sh
+++ helpers/gentitle.sh
@@ -2,11 +2,11 @@
 
 # This is a simple script which generates a 128x64 image usable as a title screen
 # for arduboy games. You pass the text and the output file. the font used is
-# the local m3x6.ttf
+# the local m3x6.ttf, or you can pass an optional third parameter
 
 # This requires imagick. It uses the new imagick command format
 
-if [ $# -ne "2" ]; then
+if [ $# -lt "2" ]; then
 	echo "You must pass in the text and the output file! Example:"
 	echo ""
 	echo "./gentitle.sh \"This is my text\nA second line\" mytitle.png"
@@ -14,4 +14,10 @@ if [ $# -ne "2" ]; then
 	exit 1
 fi
 
-magick -size 128x64 -background black -fill white -font m3x6.ttf -pointsize 16 -gravity center label:"$1" "$2"
+TITLEFONT="m3x6.ttf"
+
+if [ $# -ge "3" ]; then
+	TITLEFONT="$3"
+fi
+
+magick -size 128x64 -background black -fill white -font "$TITLEFONT" -pointsize 16 -gravity center label:"$1" "$2"
